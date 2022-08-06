9b4dd6384e1580b0f2b16533856701392eb10475
jvasquez <javierv.ramos@gmail.com>
2022-08-06 17:39:41 -0600

    RomanConvert Solution complete

--- KatasTrainings/RomanConvert.cs
+++ KatasTrainings/RomanConvert.cs
@@ -28,22 +28,56 @@ namespace KatasTrainings
         public string Solution(int number)
         {
             Dictionary<int, string> RomanMap = GetRomanMap();
+            var numberChar = number.ToString().ToArray();
+            List<int> decimalMap = new List<int>();
+            string romanNumber = "";
 
-            //ToDo
+            for ( int index = 0; index < numberChar.Length; index++)
+            {
+                decimalMap.Add(Convert.ToInt32(numberChar[index].ToString().PadRight(numberChar.Length - index, '0')));
+            }
 
-            return "";
+            foreach (int num in decimalMap)
+            {
+                romanNumber += RomanMap.GetValueOrDefault(num, "");
+            }
+
+            return romanNumber;
         }
         private Dictionary<int, string> GetRomanMap()
         {
             return new Dictionary<int, string>()
             {
                 {1, "I"},
+                {2, "II"},
+                {3, "III"},
+                {4, "IV"},
                 {5, "V"},
+                {6, "VI"},
+                {7, "VII"},
+                {8, "VIII"},
+                {9, "IX"},
                 {10, "X"},
+                {20, "XX"},
+                {30, "XXX"},
+                {40, "XL"},
                 {50, "L"},
+                {60, "LX"},
+                {70, "LXX"},
+                {80, "LXXX"},
+                {90, "XC"},
                 {100, "C"},
+                {200, "CC"},
+                {300, "CCC"},
+                {400, "CD"},
                 {500, "D"},
-                {1000, "M"}
+                {600, "DC"},
+                {700, "DCC"},
+                {800, "DCCC"},
+                {900, "CM"},
+                {1000, "M"},
+                {2000, "MM"},
+                {3000, "MMM"}
             };
         }
     }
