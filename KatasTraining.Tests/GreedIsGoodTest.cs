64f84328212364a0a82371093f6dbe234116e786
Jose Javier Vasquez Ramos <javierv.ramos@gmail.com>
2022-08-22 22:36:17 -0600

    Create GreedIsGood Game without refactor

---
+++ KatasTraining.Tests/GreedIsGoodTest.cs
@@ -0,0 +1,49 @@
+﻿using KatasTrainings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatasTraining.Tests
+{
+    public class GreedIsGoodTest
+    {
+        [Theory]
+        [InlineData(250, new int[] { 5, 1, 3, 4, 1 })]
+        [InlineData(1100, new int[] { 1, 1, 1, 3, 1 })]
+        [InlineData(450, new int[] { 2, 4, 4, 5, 4 })]
+        public void Score(int expected, int[] dice)
+        {
+            //arrange
+            GreedIsGood greedIsGood = new GreedIsGood();
+
+            //act
+            int result = greedIsGood.Score(dice);
+
+            //asserts
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void ShouldBeWorthless()
+        {
+            GreedIsGood greedIsGood = new GreedIsGood();
+            Assert.Equal(0, greedIsGood.Score(new int[] { 2, 3, 4, 6, 2 }));
+        }
+
+        [Fact]
+        public static void ShouldValueTriplets()
+        {
+            GreedIsGood greedIsGood = new GreedIsGood();
+            Assert.Equal(400, greedIsGood.Score(new int[] { 4, 4, 4, 3, 3 }));
+        }
+
+        [Fact]
+        public static void ShouldValueMixedSets()
+        {
+            GreedIsGood greedIsGood = new GreedIsGood();
+            Assert.Equal(450, greedIsGood.Score(new int[] { 2, 4, 4, 5, 4 }));
+        }
+    }
+}
---
+++ KatasTrainings/GreedIsGood.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatasTrainings
+{
+    public class GreedIsGood
+    {
+        public int Score(int[] dice)
+        {
+            int score = 0;
+            List<int> diceCopy = dice.ToList();
+            List<int> threeValueDices = new List<int>();
+
+            foreach (int numr in dice)
+            {
+                int count = dice.Where(n => n == numr).Select(n => n).Count();
+
+                if (count >= 3 && !threeValueDices.Contains(numr))
+                {
+                    for(int i = 0; i < 3; i++)
+                    {
+                        diceCopy.Remove(numr);
+                    }
+                    threeValueDices.Add(numr);
+                }
+            }
+
+            foreach(int threeVal in threeValueDices)
+            {
+                score += GetThreeValue().GetValueOrDefault(threeVal);
+            }
+
+            foreach (int oneVal in diceCopy)
+            {
+                score += GetOneValue().GetValueOrDefault(oneVal);
+            }
+
+            return score;
+        }
+
+        private Dictionary<int, int> GetThreeValue()
+        {
+            return new Dictionary<int, int>()
+            {
+                {1, 1000},
+                {6, 600},
+                {5, 500},
+                {4, 400},
+                {3, 300},
+                {2, 200}
+            };
+        }
+
+        private Dictionary<int, int> GetOneValue()
+        {
+            return new Dictionary<int, int>()
+            {
+                {1, 100},
+                {5, 50}
+            };
+        }
+    }
+}
